56740e1f2ddf25dfbcbb70d35e091028214bf3af
Jacob Samro <jacobsamro.441@gmail.com>
2015-01-03 00:12:39 +0530

    New Functions UCFIRST and UCLAST

--- funcpy.py
+++ funcpy.py
@@ -3,7 +3,7 @@
 # @Author: JacobSamro
 # @Date:   2014-12-21 15:19:44
 # @Last Modified by:   JacobSamro
-# @Last Modified time: 2014-12-21 15:55:06
+# @Last Modified time: 2015-01-03 00:12:03
 
 def strlen(string):
     return len(string)
@@ -14,15 +14,15 @@ def substr(data,start,length):
 def ucfirst(data):
 	return data[0].upper()+data[1:]
 
+def uclast(data):
+	return data[0:len(data)-1]+data[len(data)-1:].upper()
+
 def explode(splitter, string):
     if splitter == '':
         return list(string)
     else:
         return string.split(splitter, len(string))
         
-            
-# def implode(joinner, string):
-#     return string.join(joinner)
-        
+
 
     
--- test.py
+++ test.py
@@ -3,15 +3,17 @@
 # @Author: JacobSamro
 # @Date:   2014-12-21 15:21:07
 # @Last Modified by:   JacobSamro
-# @Last Modified time: 2014-12-21 15:55:13
+# @Last Modified time: 2015-01-03 00:12:10
 
 from funcpy import *
 
 print(strlen('Hello'))
-print(ucfirst('sshello'))
+print(ucfirst('hello'))
 print ( substr('Hello',1,2))
 
 data = 'He,l,lo'
+
 print ( explode(' ', data))
 
-# print(implode(' ',data))
+
+print(uclast('hello'))
