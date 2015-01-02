25cee810e5c1a4f18861aa4143210e0fa4bc8324
Jacob Samro <jacobsamro.441@gmail.com>
2015-01-03 00:24:26 +0530

    New functions max,min,mid

--- funcpy.py
+++ funcpy.py
@@ -3,26 +3,39 @@
 # @Author: JacobSamro
 # @Date:   2014-12-21 15:19:44
 # @Last Modified by:   JacobSamro
-# @Last Modified time: 2015-01-03 00:12:03
+# @Last Modified time: 2015-01-03 00:22:54
+import math
+	
+class fp(object):
+	def __init__(self, arg):
+		super(fp, self).__init__()
+		self.arg = arg
 
-def strlen(string):
-    return len(string)
+	def max(list):
+		return max(list)
 
-def substr(data,start,length):
-	return data[start:length]
+	def min(list):
+		return min(list)
 
-def ucfirst(data):
-	return data[0].upper()+data[1:]
+	def mid(list):
+		loc = math.floor((len(list)-1)/2)
+		return list[loc]
 
-def uclast(data):
-	return data[0:len(data)-1]+data[len(data)-1:].upper()
+	def strlen(string):
+		return len(string)
 
-def explode(splitter, string):
-    if splitter == '':
-        return list(string)
-    else:
-        return string.split(splitter, len(string))
-        
+	def substr(data,start,length):
+		return data[start:length]
 
+	def ucfirst(data):
+		return data[0].upper()+data[1:]
 
-    
+	def uclast(data):
+		return data[0:len(data)-1]+data[len(data)-1:].upper()
+
+	def explode(splitter, string):
+	    if splitter == '':
+	        return list(string)
+	    else:
+	        return string.split(splitter, len(string))	
+		
--- test.py
+++ test.py
@@ -3,17 +3,24 @@
 # @Author: JacobSamro
 # @Date:   2014-12-21 15:21:07
 # @Last Modified by:   JacobSamro
-# @Last Modified time: 2015-01-03 00:12:10
+# @Last Modified time: 2015-01-03 00:23:09
 
 from funcpy import *
 
-print(strlen('Hello'))
-print(ucfirst('hello'))
-print ( substr('Hello',1,2))
+print(fp.strlen('Hello'))
+print(fp.ucfirst('hello'))
+print (fp.substr('Hello',1,2))
 
 data = 'He,l,lo'
 
-print ( explode(' ', data))
+print ( fp.explode(' ', data))
 
+print(fp.uclast('hello'))
 
-print(uclast('hello'))
+nums = [1,2,3,23,53,6,7,8,9,10]
+
+print(fp.max(nums))
+
+print(fp.min(nums))
+
+print(fp.mid(nums))
